2aa1d9036c36283c1a800389284d17d2e3b4e515
SethyM12 <seth50821@outlook.com>
2026-01-04 22:06:38 -0500

    Fixed syntax error

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
     public static void main(String[] args) 
     {
-        System.oaaaaaaaut.println("Hello, world! From: The new forked repo :]");
+        System.out.println("Hello, world! From: The new forked repo :]");
     }
 }
