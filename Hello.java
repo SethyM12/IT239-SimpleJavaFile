3d9c029adfc42f5d64362a8b6b2ff8c398d49ab2
SethyM12 <seth50821@outlook.com>
2026-01-04 21:57:50 -0500

    Java syntax error

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello
 {
     public static void main(String[] args) 
     {
-        System.oaaaaut.println("Hello, world! From: The new forked repo :]");
+        System.oaaaaaaaut.println("Hello, world! From: The new forked repo :]");
     }
 }
